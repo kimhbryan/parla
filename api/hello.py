a51938ab9f6a4d169a9f642d882fd8df3f38a56d
Jeffrey <jeffrey.luo@mail.utoronto.ca>
2023-09-16 16:13:00 -0400

    added working feedback generation

--- api/hello.py
+++ api/hello.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import requests
 import cohere
+import json
 from google.cloud import speech
 from google.cloud import translate_v2 as translate
 
@@ -31,18 +32,6 @@ def chat(topic):
 
         answer = response.text
         return answer
-
-@app.route("/analyzetext")
-def analyzetext():
-    url = "https://api.languagetoolplus.com/v2/check"
-    params = {
-        "text": "J'aime les baguettes mais je n'aime pas las filles",
-        "language": "fr",
-        }
-    res = requests.post(url=url, params=params)
-
-    return res.json()
-
 
 def transcribe_audio(file_path):
     client = speech.SpeechClient()
@@ -73,6 +62,90 @@ def translate_text(target_lang, text):
 
     return result["translatedText"]
 
+@app.route("/generate")
+def generate(context: str, subject: str) -> str:
+    """ Calls cohere's generation feature with a strict context-subject format
+
+    Parameters
+    context: The context in which the AI should generate a response
+    subject: The actual subject that the AI is considering
+
+    Returns:
+    The generated response from cohere in string format
+    """
+    prompt = context + " " + subject
+    response = cohere.generate(
+        prompt,
+        model="command",
+        max_tokens=200          
+    )
+
+    generated = ""
+
+    for gen in response.generations:
+        generated += gen.text
+
+    return generated
+
+@app.route("/analyze")
+def analyzetext(text: str, lang: str) -> (bool, str):
+    """ Calls languagetoolplus's API to detect grammar errors
+
+    Parameters
+    text: The text that should be analyzed
+    lang: the language of the text
+
+    Returns:
+    A tuple (bool, err) where the first value represents the existence
+    of an error, and the second describes it if it exists
+    """
+    url = "https://api.languagetoolplus.com/v2/check"
+    params = {
+        "text": text,
+        "language": lang,
+        }
+    res = requests.post(url=url, params=params)
+
+    grammar_err = False
+    err_desc = ""
+    if len(res.json()["matches"]) > 0:
+        grammar_err = True
+        for err in res.json()["matches"]:
+            err_desc += err["message"]
+
+    return (grammar_err, err_desc)
+
+@app.route("/feedback")
+def feedback(user_transcript=[], lang="en") -> json:
+    """Generates feedback for the user based on the transcript of the conversation
+
+    Parameters
+    user_transcript: An array consisting of each paragraph the user has spoken
+    lang: The language that the user has spoken
+
+    Returns:
+    A json dict with recommendations for each erroneous paragraph with the index as the key, 
+    as well as an overall recommendation corresponding to the key "overall"
+    """
+    context = "Give a correct version of the following grammatically incorrect paragraph:"
+    recommendations = dict()
+    comments = ""
+
+    # add recommendations for specific sections
+    for id, seq in enumerate(user_transcript):
+        err = analyzetext(seq, lang)    
+        if err[0]:
+            recommendations[id] = generate(context, seq)
+            comments += err[1]
+
+    # add overall recommendation
+    context = "Give an short summarized overall recommendation only for a person in order to improve their speaking based on the following obervations about a person's speaking:"
+
+    if len(recommendations) >= 1:
+        overall_rec = generate(context, comments)
+        recommendations["overall"] = overall_rec
+
+    return json.dumps(recommendations)
 
 if __name__ == '__main__':
     app.run()
